36f1ce6f5dc21871d59c757e11970fb353d4935a
Aiza Gazyani <167635394+Aiza166@users.noreply.github.com>
2025-08-13 00:49:42 +0500

    Time: 35 ms (92.6%), Space: 54.2 MB (25.51%) - LeetHub

--- 2797-event-emitter/2797-event-emitter.js
+++ 2797-event-emitter/2797-event-emitter.js
@@ -1,39 +1,54 @@
 class EventEmitter {
-  constructor() {
-    this.events = new Map();
-  }
-
-  subscribe(event, cb) {
-    if (!this.events.has(event)) {
-      this.events.set(event, []);
+    constructor() {
+        this.events = new Map();
     }
-
-    const listeners = this.events.get(event);
-    listeners.push(cb);
-
-    return {
-      unsubscribe: () => {
-        const index = listeners.indexOf(cb);
-        if (index !== -1) {
-          listeners.splice(index, 1);
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+        if(!this.events.has(eventName)) {
+            this.events.set(eventName, [])
         }
-      }
-    };
-  }
 
-  emit(event, args = []) {
-    if (!this.events.has(event)) {
-      return [];
+        const listeners = this.events.get(eventName)
+        listeners.push(callback)
+
+        return {
+            unsubscribe: () => {
+                const index = listeners.indexOf(callback)
+                if (index !== -1) listeners.splice(index, 1)
+            }
+        };
     }
 
-    const listeners = this.events.get(event);
-    const results = [];
-
-    for (const listener of listeners) {
-      results.push(listener(...args));
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+       if (!this.events.has(eventName)) {
+        return []
+       }
+       const listeners = this.events.get(eventName)
+       const result = [];
+       for (const listener of listeners) {
+            result.push(listener(...args))
+       }
+       return result
     }
-
-    return results;
-  }
 }
 
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
